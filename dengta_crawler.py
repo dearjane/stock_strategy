d2f1bbb6135229220c64cba44f7733888c0bb270
Toby <licb@cnns.net>
2018-03-09 17:17:43 +0800

    U: 增加灯塔历史推荐列表计算收益的功能

--- dengta_crawler.py
+++ dengta_crawler.py
@@ -3,9 +3,13 @@ import time
 import json
 import logging
 import datetime as dt
+from functools import lru_cache
+from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
 
+import tushare
 import requests
+from pandas import DataFrame
 from dateutil.parser import parse as dt_parse
 
 from models import RecommendSource, StockRecommended, WinRateDaily
@@ -30,6 +34,24 @@ def intellistock_crawler_task():
             executor.submit(task)
 
 
+@lru_cache()
+def calculate_profit(code, buy_date, keep_days=1):
+    '''计算收益百分比，购买日期第二天开盘价减去当天开盘价
+    :param code: 代码
+    :param buy_date: 购买日期, 格式为%Y-%m-%d
+    :param keep_days: 持股天数
+    '''
+    data = tushare.get_k_data(code, start=buy_date)
+    # 尝试获取卖出日期的股票信息,如果没有对应信息则无法计算收益,返回None
+    try:
+        data.iloc[keep_days]
+    except IndexError:
+        return None
+    last_open_price = data.iloc[keep_days]['open']
+    first_open_price = data.iloc[0]['open']
+    return (last_open_price - first_open_price)/first_open_price
+
+
 class RecommendSourceCrawler():
     '''策略精选爬虫类'''
 
@@ -44,6 +66,36 @@ class RecommendSourceCrawler():
         data = json.loads(text)
         content = json.loads(data['content'])
         return content['stIntelliPickStockV2']
+
+    @staticmethod
+    def get_recommend_history(source_id):
+        INTELLISTOCK_API_PAT = 'https://sec.wedengta.com/getIntelliStock?action=IntelliSecPool&id={source_id}&_={t}'
+        intellistock_api = INTELLISTOCK_API_PAT.format(source_id=source_id, t=now_timestamp())
+        text = requests.get(intellistock_api).text
+        data = json.loads(text)
+        content = json.loads(data['content'])
+        return content['vtDaySec']
+
+    def simulate_history_profit(self, keep_days=1, service_charge=0):
+        '''获取股票灯塔策略推荐历史，并模拟买卖计算收益
+        :param keep_days: 持股天数
+        :param service_charge: 手续费率(主要为佣金), 百分比
+        '''
+        content = self.get_recommend_history(self.r_source_model.source_id)
+        # key为对应的列名，value为对应列的值列表
+        history_map = defaultdict(list)
+        for item in content:
+            for stock in item['vtSec']:
+                code = stock['sDtCode'][4:]
+                recommend_day = item['sOptime']
+                history_map['code'].append(code)
+                history_map['name'].append(stock['sChnName'])
+                history_map['date'].append(recommend_day)
+                history_map['profit'].append(calculate_profit(code, recommend_day, keep_days))
+                history_map['hs300_profit'].append(calculate_profit('hs300', recommend_day, keep_days))
+                history_map['keep_days'] = keep_days
+                history_map['service_charge'] = service_charge
+        return DataFrame(history_map)
 
     def run(self):
         logger.info(self.r_source_model)
@@ -68,6 +120,9 @@ class RecommendSourceCrawler():
                 stock=stock['sSecName'],
                 code=stock['sDtSecCode'][4:],
                 )
+
+def test():
+    print(2)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
